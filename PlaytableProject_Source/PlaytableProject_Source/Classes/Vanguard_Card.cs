1fb15ac7660daefba8195e201b24249555277006
Isaac Anderson <phirephox4d@gmail.com>
2013-01-22 00:46:41 -0500

    Setting up base classes
    Made Card class and enums...

---
+++ PlaytableProject_Source/PlaytableProject_Source/Classes/Vanguard_Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaytableProject_Source.Classes
+{
+
+    class Vanguard_Card
+    {
+        public string name { get; }
+        public int power { get; }
+        public string critcal  { get; }
+        public Nation nation { get; }
+        public Clan clan { get; }
+        public Race race { get; }
+        public Unit_Type uType { get; }
+        public string box_Set { get; }
+        public int card_Number { get; }
+        public int grade { get; }
+        public Skill skill { get; }
+        public string flavor_Text { get; }
+        public string rule_Text { get; }
+        public Trigger_Type tType { get; }
+        public Rarity rarity { get; }
+    }
+}
---
+++ PlaytableProject_Source/PlaytableProject_Source/Enums/Vanguard_Constants.cs
@@ -0,0 +1,108 @@
+﻿enum Rarity
+{
+    C = "commom",
+    R = "Rare",
+    RR = "Double Rare",
+    RRR = "Triple Rare",
+    SP = "Special Parallel"
+}
+enum Nation
+{
+    United_Sanctuary = "United Sanctuary",
+    Dragon_Empire = "Dragon Empire",
+    Dark_Zone = "Dark Zone",
+    Magallanica = "Magallanica",
+    Zoo = "Zoo",
+    Star_Gate = "Star Gate"
+}
+
+enum Clan
+{
+    Royal_Paladin = "Royal Paladin",
+    Shadow_Paladin = "Shadow Paladin",
+    Gold_Paladin = "Gold Paladin",
+    Oracle_Think_Tank = "Oracle Think Tank",
+    Angel_Feather = "Angel Feather",
+    Kagero = "Kagero",
+    Murakumo = "Murakumo",
+    Narukami = "Narukami",
+    Nubatama = "Nubatama",
+    Tachikaze = "Tachikaze",
+    Dark_Irregulars = "Dark Irregulars",
+    Pale_Moon = "Pale Moon",
+    Spike_Brothers = "Spike Brothers",
+    Aqua_Force = "Aqua Force",
+    Bermuda_Triangle = "Bermuda Triangle",
+    Granblue = "Granblue",
+    Great_Nature = "Great Nature",
+    Megacolony = "Megacolony",
+    Neo_Nectar = "Neo Nectar",
+    Dimension_Police = "Dimension Police",
+    Etranger = "Etranger",
+    Nova_Grappler = "Nova Grappler"
+}
+
+enum Race
+{
+    Abyss_Dragon = "Abyss Dragon",
+    Alien = "Alien",
+    Angel = "Angel",
+    Battleroid = "Battleroid",
+    Chimera = "Chimera",
+    Cosmo_Dragon = "Cosmo Dragon",
+    Demon = "Demon",
+    Dragonman = "Dragonman",
+    Elf = "Elf",
+    Flame_Dragon = "Flame_Dragon",
+    Giant = "Giant",
+    Ghost = "Ghost",
+    Gillman = "Gillman",
+    Gnome = "Gnome",
+    Goblin = "Goblin",
+    Golem = "Golem",
+    High_Beast = "High Beast",
+    Human = "Human",
+    Insect = "Insect",
+    Mermaid = "Mermaid",
+    Noble = "Noble",
+    Ogre = "Orge",
+    Salamander = "Salamander",
+    Succubus = "Succubus",
+    Sylph = "Sylph",
+    Tear_Dragon = "Tear Dragon",
+    Vampire = "Vampire",
+    Warbeast = "Warbeast",
+    Winged_Dragon = "Winged Dragon",
+    Workeroid = "Workeroid",
+    Aquaroid = "Aquaroid",
+    Bioroid = "Bioroid",
+    Dino_Dragon = "Dino Dragon",
+    Dryad = "Dryad",
+    Forest_Dragon = "Forest Dragon",
+    Kraken = "Kraken",
+    Shadow_Dragon = "Shadow Dragon",
+    Skeleton = "Skeleton",
+    Thunder_Dragon = "Thunder Dragon",
+    Zombie = "Zombie"
+}
+
+enum Unit_Type
+{
+    Normal_Unit = "Normal",
+    Trigger_Unit = "Trigger"
+}
+
+enum Skill
+{
+    Boost = "Boost",
+    Intercept = "Intercept",
+    Twin_Drive = "Twin Drive"
+}
+
+enum Trigger_Type
+{
+    Stand,
+    Draw,
+    Critical,
+    Heal
+}
